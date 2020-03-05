5e01160dc6dbdda4226652e116861ccb5e45bd17
skuldx <61177084+skuldx@users.noreply.github.com>
2020-03-05 14:32:40 +0800

    Update hw_week4.py

--- hw_week4.py
+++ hw_week4.py
@@ -1,6 +1,20 @@
 def hw_week4(name, age):
     # Your code here
-    pass
+    name = input('Please input your name:')
+    age = input('Please input your age:')
+
+    if name == 'Alice':
+        print('Hi,Alice.')
+    elif int(age) < 12:
+        print('You are not Alice, kiddo.')
+    
+    elif int(age) > 2000:
+        print('Unlike you, Alice is not an undead, imortal vampire.')
+    
+    elif int(age) > 100:
+        print('You are not Alice, grannie.')
+    
+    print('No suitable result, program ends')
 
 
 if __name__ == '__main__':
